304490bd08db82d893b5c66c2a8c1cd12b7f8d15
wuwencha0 <283157546@qq.com>
2017-09-25 16:30:19 +0800

    新建shangxin.vue

---
+++ src/components/shangxin.vue
@@ -0,0 +1,20 @@
+<template>
+ <div>
+       
+   </div>
+</template>
+ 
+<script>
+export default {
+  name: "component_name",
+  data () {
+    return {
+        aa: 123
+    };
+  }
+}
+</script>
+ 
+<style lang="css" scoped>
+ 
+</style>
