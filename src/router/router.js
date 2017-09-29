827e56e90a81acad951a8309949e8a99331fd5a7
wuwencha0 <283157546@qq.com>
2017-09-29 14:36:06 +0800

    分离路由，安装axios,vuex

---
+++ src/router/router.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+
+//导入路由模块
+import VueRouter from 'vue-router'
+//全局安装路由
+Vue.use(VueRouter);
+
+//导入一级组件
+import Today from '../pages/today/Today'
+import Youxuan from '../pages/youxuan/Youxuan'
+import Global from '../pages/global/Global'
+import Cart from '../pages/cart/Cart'
+import Mine from '../pages/mine/Mine'
+import IndexNav from '../pages/public/IndexNav'
+
+//导入二级组件
+import Shangxin from '../pages/today/Shangxin'
+import Nvzhuang from '../pages/today/Nvzhuang'
+import Shoping from '../pages/youxuan/Shoping'
+import Live from '../pages/youxuan/Live'
+
+//配置路由规则
+const routes=[
+  { path: '' , component: Today, children: [
+    { path: '', component: Shangxin },
+    { path: 'shangxin', component: Shangxin },
+    { path: 'nvzhuang', component: Nvzhuang }
+  ] },
+  { path: '/today' , component: Today, children: [
+    { path: '', component: Shangxin },
+    { path: 'shangxin', component: Shangxin },
+    { path: 'nvzhuang', component: Nvzhuang }
+  ] },
+  { path: '/youxuan' , component: Youxuan, children: [
+    {path: '', component: Shoping},
+    {path: 'shoping', component: Shoping},
+    {path: 'live', component: Live}
+  ]},
+  { path: '/global' , component: Global },
+  { path: '/cart' , component: Cart },
+  { path: '/mine' , component: Mine }
+]
+
+//创建路由实例
+const router = new VueRouter({
+  routes
+})
+
+//导出路由
+export default router
---
+++ src/store/store.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+//全局注册
+Vue.use(Vuex)
+
+let state = {
+
+}
+
+let mutations = {
+
+}
+
+let actions = {
+
+}
+
+let getters = {
+
+}
+
+const store = new Vuex.Store({
+    state,
+    mutations,
+    actions,
+    getters
+})
+
+export default store
