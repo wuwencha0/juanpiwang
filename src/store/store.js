4c9d998fd69ecd3d45d0de40c9ec66b082b48090
wuwencha0 <283157546@qq.com>
2017-10-19 20:48:54 +0800

    仓库

--- src/store/store.js
+++ src/store/store.js
@@ -5,11 +5,38 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 let state = {
-
+    cart_goods: [],
+    current_good: {},
+    user: {}
 }
 
 let mutations = {
+    ADD(state, good){
+        // 用来判断所添加的商品是否存在的标识
+        let isExist = false
 
+        // 存在的话，把当前购物车中的商品对象数量+1
+        for (let tempItem of state.cart_goods) {
+            if (tempItem.id == good.id) {
+                // 把标识改成true
+                isExist = true
+                tempItem.count++
+                break;
+            }
+        }
+
+        // 如果不存在，则是新添加的商品
+        if (!isExist) {
+            // 设计购物车商品对象
+            let good = {
+                id: item.id,
+                name: item.name,
+                price: item.price,
+                count: 1
+            }
+            state.cartProductList.push(cartProduct)
+        }
+    }
 }
 
 let actions = {
